83f82e749d6d5775c175b39f34ad260b06a8e6bb
tylerchenoweth <32681915+tylerchenoweth@users.noreply.github.com>
2025-01-24 11:23:52 -0500

    removed the nav elements

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,13 +6,6 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Sammy the Weather Swami</h1>
-      <nav>
-        <ul className="nav-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
     </header>
   );
 };
