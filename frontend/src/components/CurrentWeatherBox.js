d59b6b518a72fa3b21d41b71f84ad991791870ce
tylerchenoweth <32681915+tylerchenoweth@users.noreply.github.com>
2025-01-24 16:15:29 -0500

    changed some info that is displayed

--- frontend/src/components/CurrentWeatherBox.js
+++ frontend/src/components/CurrentWeatherBox.js
@@ -5,10 +5,10 @@ import './styles/Header.css'; // Import CSS file for styling (optional)
 const CurrentWeatherBox = ({geo, now}) => {
   return (
     <div>
-      <h2>{geo.city}, {geo.state}</h2>
-      <h2>{geo.country}</h2>
-      <h2><b>Temperature: </b>{now.temp} <span>&#8457;</span></h2>
-      <h2><b>Feels Like: </b>{now.feels_like} <span>&#8457;</span></h2>
+      <h2>{geo.city}, {geo.state} {geo.country}A!, USA!</h2>
+
+      <h3><b>Temperature: </b>{now.temp} <span>&#8457;</span></h3>
+      <h3><b>Feels Like: </b>{now.feels_like} <span>&#8457;</span></h3>
     </div>
   );
 };
