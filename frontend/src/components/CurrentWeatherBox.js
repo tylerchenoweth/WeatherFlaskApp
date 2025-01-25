998fb1ace827b60a2c5c1ab5c482ef1bf500298a
sh4ck1et0n <tchen358@gmail.com>
2025-01-25 14:38:19 -0500

    added the imports for a stylesheet

--- frontend/src/components/CurrentWeatherBox.js
+++ frontend/src/components/CurrentWeatherBox.js
@@ -1,12 +1,11 @@
 // src/components/Header.js
 import React from 'react';
-import './styles/Header.css'; // Import CSS file for styling (optional)
+import styles from './styles/CurrentWeatherBox.css';
 
 const CurrentWeatherBox = ({geo, now}) => {
   return (
-    <div>
+    <div className="currentWeatherMain">
       <h2>{geo.city}, {geo.state} {geo.country}A!, USA!</h2>
-
       <h3><b>Temperature: </b>{now.temp} <span>&#8457;</span></h3>
       <h3><b>Feels Like: </b>{now.feels_like} <span>&#8457;</span></h3>
     </div>
