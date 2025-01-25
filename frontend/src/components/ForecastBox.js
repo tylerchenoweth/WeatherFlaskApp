79b755167d847d88f8f515ba3e85b506f0b528e6
sh4ck1et0n <tchen358@gmail.com>
2025-01-25 14:39:24 -0500

    fixed the naming of the function and stylesheet

--- frontend/src/components/ForecastBox.js
+++ frontend/src/components/ForecastBox.js
@@ -1,21 +1,20 @@
-// src/components/Header.js
 import React from 'react';
-import './styles/ForecastBox.css'; // Import CSS file for styling (optional)
+import styles from './styles/ForecastBox.css';
 
-const CurrentWeatherBox = ({forecast}) => {
-
+const ForecastBox = ({forecast}) => {
 
   return (
     <div className="box">
       <h2>{forecast.day}</h2>
+      <img src={forecast.weather.icon}></img>
       {/* <p><b>Temperature: </b>{forecast.temp} <span>&#8457;</span></p> */}
-      <p>{forecast.min_temp} --- {forecast.max_temp}</p>
+      <p>{forecast.min_temp} <span>&#8457;</span> --- {forecast.max_temp} <span>&#8457;</span></p>
       {/* <p><b>Feels Like: </b>{forecast.feels_like} <span>&#8457;</span></p> */}
       <p><b>Humidity: </b>{forecast.humidity}%</p>
-      <img src={forecast.weather.icon}></img>
+      
       {/* <p>{forecast.weather.description}</p> */}
     </div>
   );
 };
 
-export default CurrentWeatherBox;
+export default ForecastBox;
