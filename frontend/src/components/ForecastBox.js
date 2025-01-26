b8e10b18536a746237cb6066f5a5f6aa5a94109c
sh4ck1et0n <tchen358@gmail.com>
2025-01-25 21:24:29 -0500

    added if statement that will print 'today' instead of the day of the week

--- frontend/src/components/ForecastBox.js
+++ frontend/src/components/ForecastBox.js
@@ -3,9 +3,23 @@ import styles from './styles/ForecastBox.css';
 
 const ForecastBox = ({forecast}) => {
 
+  const today = new Date();
+  const savedDate = new Date(forecast.datetime)
+
+  let isToday = false;
+
+  if(savedDate.getDate() == today.getDate()) {
+    if(savedDate.getMonth() == today.getMonth()) {
+      if(savedDate.getYear() == today.getYear()) {
+        isToday = true
+      }
+    }
+  }
+
   return (
     <div className="box">
-      <h2>{forecast.day}</h2>
+
+      {isToday ? <h2>Today</h2> : <h2>{forecast.day}</h2>}
       <img src={forecast.weather.icon}></img>
       {/* <p><b>Temperature: </b>{forecast.temp} <span>&#8457;</span></p> */}
       <p>{forecast.min_temp} <span>&#8457;</span> --- {forecast.max_temp} <span>&#8457;</span></p>
