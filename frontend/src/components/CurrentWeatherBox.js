36cc598c8e8ef9ae613bb516a39d401f87e6b4bb
sh4ck1et0n <tchen358@gmail.com>
2025-01-25 20:55:53 -0500

    got rid of a  comma

--- frontend/src/components/CurrentWeatherBox.js
+++ frontend/src/components/CurrentWeatherBox.js
@@ -5,7 +5,7 @@ import styles from './styles/CurrentWeatherBox.css';
 const CurrentWeatherBox = ({geo, now}) => {
   return (
     <div className="currentWeatherMain">
-      <h2>{geo.city}, {geo.state} {geo.country}A!, USA!</h2>
+      <h2>{geo.city}, {geo.state} {geo.country}A! USA!</h2>
       <h3><b>Temperature: </b>{now.temp} <span>&#8457;</span></h3>
       <h3><b>Feels Like: </b>{now.feels_like} <span>&#8457;</span></h3>
     </div>
