c4f3ce6aa4eda7a957e151d53c1c21f7efb7080a
tylerchenoweth <32681915+tylerchenoweth@users.noreply.github.com>
2025-01-31 15:57:40 -0500

    when an API call is made the data will automatically be saved to a JSON file

--- backend/routes.py
+++ backend/routes.py
@@ -7,7 +7,14 @@ import os, json
 from flask import jsonify
 from flask_cors import cross_origin
 
+# API urls
+# http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+# https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API key}
 
+
+# Hard coded coordinates for Tampa
+# lat = 27.9574621
+# long = -82.4597585
 
 def kelvin_to_fahrenheit(kelvin):
     kelvin = float(kelvin)
@@ -26,64 +33,17 @@ def epoch_to_day_of_week(epoch_time):
 def epoch_to_datetime(epoch_time):
     return datetime.fromtimestamp(epoch_time)
 
+
 def epoch_to_standard_time(epoch_time):
     return datetime.fromtimestamp(epoch_time).strftime("%I:%M:%S %p")
 
-def getLatLongByCity(cityName, api_key):
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&appid={api_key}"
-    response = requests.get(url)
-    data = response.json()
 
-    # Check if the city is in a state
-    try:
-        state = data[0]['state']
-    except:
-        state = None
-
-    geo = {
-        "city": data[0]['name'],
-        "state": state,
-        "country": data[0]['country'],
-        "lat": data[0]['lat'],
-        "long": data[0]['lon'],
-    }
-
-    return geo
+def writeToFile(data, fileName):
+    with open(fileName, "w") as file:
+        json.dump(data, file, indent=4)
 
 
-def getWeatherDataWLatLong(lat, long, api_key):
-    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&appid={api_key}&units=imperial"
-    response = requests.get(url)
-    data = response.json()
-
-    return data
-
-
-
-@app.route('/')
-@cross_origin(origin='http://localhost:3000')
-def index():
-    
-    # Hard coded coordinates for Tampa
-    # lat = 27.9574621
-    # long = -82.4597585
-
-    # https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={API key}
-
-    # This url is used to get lat and long coords by city name
-    # http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-
-    # url = "https://api.openweathermap.org/data/3.0/onecall?lat="+str(lat)+"&lon="+str(long)+"&appid="+API_Key
-
-
-    #########################################################################################################################
-
-
-    # load_dotenv()
-    # api_key = os.getenv('API_KEY')
-    
-    api_key = ""
-
+def getSelectedForecastData(data):
     # The weather icon will be put between these two elements
     # to get the full URL
     icon_url = [
@@ -91,36 +51,8 @@ def index():
         "@2x.png"
     ]
 
-
-    # Declare blank variables to load later
-
-    # Holds all the data retrieved from the API
-    data = {}
-    # Holds geographic data of the city
-    geo = {}
-    # Holds the data for the future weather
     forecast = {}
-    # Holds data{}, forecast{}, now{} and gets returned
-    context = {}
 
-    # Determine if were working with current or offline data
-    # If there is no API Key then just use offline data
-    if(api_key == ""):
-        # Load data from json file when working without api requests
-        with open('./offlineData.json', 'r') as file:
-            fileData = json.load(file)
-
-            geo = fileData['CityDetails']
-            data = fileData['WeatherData']
-    else:
-        # Get basic info about the city
-        geo = getLatLongByCity("tampa", api_key)
-
-        # Get weather data
-        data = getWeatherDataWLatLong(geo['lat'], geo['long'], api_key)
-
-
-    # Get the forecast from the json
     for num, i in enumerate(data['daily']):
         forecast[num] = {
             "day": epoch_to_day_of_week(i['dt']),
@@ -143,7 +75,37 @@ def index():
                 "icon": icon_url[0] + i['weather'][0]['icon'] + icon_url[1]
             },
             "summary": i['summary']
-        }  
+        } 
+
+    return forecast
+
+
+def getSelectedGeoData(data):
+    # Check if the city is in a state
+    try:
+        state = data[0]['state']
+    except:
+        state = None
+
+    # Write the most recent data to a json file
+    writeToFile(data, "sampleData_City.json")
+
+    geo = {
+        "city": data['name'],
+        "state": state,
+        "country": data['country'],
+        "lat": data['lat'],
+        "long": data['lon'],
+    }
+
+    return geo
+
+
+def getSelectedCurrentData(data):
+    icon_url = [
+        "https://openweathermap.org/img/wn/",
+        "@2x.png"
+    ]
 
     # Check if there are weather alerts
     try:
@@ -169,13 +131,86 @@ def index():
         }
     }
 
+    return now
+
+
+def getLatLongByCity(cityName, api_key):
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    print(data[0])
+
+    return data[0]
+
+
+def getWeatherDataWLatLong(lat, long, api_key):
+    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={long}&appid={api_key}&units=imperial"
+    response = requests.get(url)
+    data = response.json()
+
+    # Write the most recent data to a json file
+    writeToFile(data, "sampleData_Weather.json")
+
+    return data
+
+
+
+
+
+@app.route('/')
+@cross_origin(origin='http://localhost:3000')
+def index():
+
+    # load_dotenv()
+    # api_key = os.getenv('API_KEY')
+
+    api_key = ""
+
+    # Determine if were working with current or offline data
+    # If there is no API Key then just use offline data
+    if(api_key == ""):
+
+        # Load data from json file when working without api requests
+        with open('./offlineData.json', 'r') as file:
+            fileData = json.load(file)
+
+            geo = fileData['CityDetails']
+            data = fileData['WeatherData']
+
+    else:
+        # Get basic info about the city
+        geo = getLatLongByCity("tampa", api_key)
+
+        # Get weather data
+        data = getWeatherDataWLatLong(geo['lat'], geo['lon'], api_key)
+
+        # Write the City data to a json file
+        with open("sampleData_City.json", "r") as file:
+            city_data = json.load(file)
+
+        # Write the Weather data to a json file
+        with open("sampleData_Weather.json", "r") as file:
+            weather_data = json.load(file)
+
+        # Put data in a dict to write to a file
+        all_data = {
+            "CityDetails": city_data,
+            "WeatherData": weather_data
+        }
+
+        # Consolidate all data to one json file
+        with open("offlineData.json", "w") as file:
+            json.dump(all_data, file, indent=4) 
+
+    # Holds data{}, forecast{}, now{} and gets returned
+    context = {}
+
     # Load context
-    context['now'] = now
-    context['forecast'] = forecast
-    context['geo'] = geo
+    context['now'] = getSelectedCurrentData(data)
+    context['forecast'] = getSelectedForecastData(data)
+    context['geo'] = getSelectedGeoData(geo)
     
     
-    # return data
     return jsonify(context)
-    # return forecast
     
